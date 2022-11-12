fe4f69bb0b9f25fdd31db9d15abf69a498b9c72e
Deng Yonghao <dengyonghao2001@163.com>
2022-11-12 20:08:37 +0800

    increase client.c

---
+++ client/client.c
@@ -0,0 +1,15 @@
+/*********************************************************************************
+ *      Copyright:  (C) 2022 Deng Yonghao<dengyonghao2001@163.com>
+ *                  All rights reserved.
+ *
+ *       Filename:  client.c
+ *    Description:  This file 
+ *                 
+ *        Version:  1.0.0(2022年11月12日)
+ *         Author:  Deng Yonghao <dengyonghao2001@163.com>
+ *      ChangeLog:  1, Release initial version on "2022年11月12日 20时06分58秒"
+ *                 
+ ********************************************************************************/
+
+
+#define	MSG			/*  */
