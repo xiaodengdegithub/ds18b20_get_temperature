1a84e5787353da9a8b8f071850b7355ffcb78524
Deng Yonghao <dengyonghao2001@163.com>
2022-11-23 21:45:24 +0800

    client sqlite

---
+++ client/clisql.c
@@ -0,0 +1,73 @@
+/*********************************************************************************
+ *      Copyright:  (C) 2022 Deng Yonghao<dengyonghao2001@163.com>
+ *                  All rights reserved.
+ *
+ *       Filename:  clisql.c
+ *    Description:  This file server sqlite.c
+ *                 
+ *        Version:  1.0.0(2022年11月22日)
+ *         Author:  Deng Yonghao <dengyonghao2001@163.com>
+ *      ChangeLog:  1, Release initial version on "2022年11月22日 11时23分55秒"
+ *                 
+ ********************************************************************************/
+
+#include "clisql.h"
+
+/*Creat Database*/
+int Create_Database(sqlite3 **db)//如果仅仅是*db,main函数的db不会变，所以创建表时出错，改为**db
+{
+   char *zErrMsg = 0;
+   int rc;
+
+   rc = sqlite3_open("tempdata.db", db);
+
+   if( rc != SQLITE_OK ){
+      log_error("Can't open database\n");
+      return -1;
+   }else{
+      log_info("Opened database successfully\n");
+	  return 0;
+   }
+}
+
+static int callback(void *NotUsed, int argc, char **argv, char **azColName)
+{
+   int i;
+   for(i=0; i<argc; i++){
+      log_info("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+   }
+   return 0;
+}
+
+int Create_Table(sqlite3 *db)
+{
+	char			buf[256];
+ 	char			*zErrMsg = 0;
+
+	memset(buf, 0, sizeof(buf));
+	snprintf(buf, sizeof(buf), "CREATE TABLE client_table (SN CHAR(20), DATIME CHAR(100), TEMPERATURE FLOAT);");
+	log_info("buf:%s\n", buf);
+
+	if(SQLITE_OK != sqlite3_exec(db, buf, callback, 0, &zErrMsg))
+	{
+		log_error("create table error:%s\n", zErrMsg);
+		sqlite3_free(zErrMsg);
+		return -2;
+	}
+	else
+	{
+		log_info("creat table successfully\n");
+		return 0;
+	}
+}
+
+int Insert_Table(sqlite3 *db, char *SN, char *datime, float temp)
+{
+	char			buf[256];
+	char 			*zErrMsg = 0;
+
+	snprintf(buf, 256, "INSERT INTO %s(SN, DATIME, TEMPERATURE)VALUES('%s', '%s', '%f');", SN, SN, datime, temp);
+ 
+}
+
+
---
+++ client/clisql.h
@@ -0,0 +1,37 @@
+/********************************************************************************
+ *      Copyright:  (C) 2022 Deng Yonghao<dengyonghao2001@163.com>
+ *                  All rights reserved.
+ *
+ *       Filename:  clisql.h
+ *    Description:  This file client sqlite.h
+ *
+ *        Version:  1.0.0(2022年11月22日)
+ *         Author:  Deng Yonghao <dengyonghao2001@163.com>
+ *      ChangeLog:  1, Release initial version on "2022年11月22日 11时21分57秒"
+ *                 
+ ********************************************************************************/
+
+#ifndef _SERSQL_H_
+#define _SERSQL_H_
+
+#include <sqlite3.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include "logger.h"
+
+#define CONFIG_DEBUG
+#ifdef  CONFIG_DEBUG
+#define dbg_print(format,args...) printf(format, ##args)
+#else      /* -----  not CONFIG_DEBUG  ----- */
+#define dbg_print(format,args...) do{} while(0)
+#endif     /* -----  not CONFIG_DEBUG  ----- */
+
+int Create_Database(sqlite3 **db);
+static int callback(void *NotUsed, int argc, char **argv, char **azColName);
+int Create_Table(sqlite3 *db);
+int Insert_Table(sqlite3 *db, char *SN, char *datime, float temp);
+
+#endif
